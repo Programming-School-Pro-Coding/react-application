cd39c3f883d15e11c0586a42670e0c7c937d4978
Programing-School <mohamedehabam99@hotmail.com>
2022-01-25 13:15:54 +0200

    comet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import logo from "./assets/moh.png";
+
+const Langauges_I_Learned = ["Html, ", "CSS, ", "Javascript, ", "PHP, ", "MYSQL, ", "C#"];
+const FrameWorks_I_Learned = ["Nodejs, ", "Reactjs, ", "Nextjs "];
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 bg-gray-dark text-white mt-56">
+        <img
+          class="w-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+          src={logo}
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <blockquote>
+            <p class="text-lg font-medium">
+              Hello 👋, I am a Full-Stack Developer, I Learn{" "}
+              {Langauges_I_Learned}, And FrameWorks Like {FrameWorks_I_Learned}
+            </p>
+          </blockquote>
+          <figcaption class="font-medium">
+            <div class="text-sky-500 dark:text-sky-400">Mohamed Ehab</div>
+            <div class="text-slate-700 dark:text-slate-500">
+              Full-Stack Developer, Egypt
+            </div>
+          </figcaption>
+        </div>
+      </figure>
     </div>
   );
-}
+};
 
 export default App;
